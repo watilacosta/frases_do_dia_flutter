988e5ed1104de1db501d56e6de31864460f06856
Wátila Costa <watilafreire26@gmail.com>
2021-03-13 09:40:22 -0300

    creating function to generate frase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -15,6 +16,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _frases = [
+    "Nunca deixe ninguém te dizer que não pode fazer alguma coisa. Se você tem um sonho tem que correr atrás dele. As pessoas não conseguem vencer e dizem que você também não vai vencer. Se você quer uma coisa corre atrás.",
+    "As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.",
+    "Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.",
+    "Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.",
+    "A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.",
+    "A amizade desenvolve a felicidade e reduz o sofrimento, duplicando a nossa alegria e dividindo a nossa dor."
+  ];
+
+  var _fraseGerada = 'Clique no botão para gerar uma nova frase.';
+
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +49,7 @@ class _HomeState extends State<Home> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Image.asset('images/logo.png'),
-              Text('Clique no botão para gerar uma nova frase.',
+              Text(_fraseGerada,
                 textAlign: TextAlign.justify,
                 style: TextStyle(
                     fontSize: 18,
@@ -48,7 +67,7 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 color: Colors.green,
-                onPressed: (){},
+                onPressed: _gerarFrase,
               )
             ],
           ),
